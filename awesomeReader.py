2cf20e304f123ad03969fde087b5093fe1f986d0
FrancisChDour <fdourlensmonchy@gmail.com>
2021-06-08 09:20:23 +0200

    awesomeReader - GPS

--- awesomeReader.py
+++ awesomeReader.py
@@ -1,10 +1,39 @@
-import socket
+#!/usr/bin/env python3
+
+from marvelmind import MarvelmindHedge
+from time import sleep
+import sys
+import serial
 
 def main():
-    # create an INET, STREAMing socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # now connect to the web server on port 80 - the normal http port
-    s.connect(("www.python.org", 80))
+    readGPS();
+
+def readGPS():
+    hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=None, debug=False) # create MarvelmindHedge thread
+    
+    if (len(sys.argv)>1):
+        hedge.tty= sys.argv[1]
+    
+    hedge.start() # start thread
+    while True:
+        try:
+            hedge.dataEvent.wait(1)
+            hedge.dataEvent.clear()
+
+            if (hedge.positionUpdated):
+                hedge.print_position()
+                
+        except KeyboardInterrupt:
+            hedge.stop()  # stop and close serial port
+            sys.exit()
+
+def readGyro():
+    ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+    ser.flush()
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
 
 
 main()
