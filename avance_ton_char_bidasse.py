d65ae0edea05c93bb1eb3e29a7d09fd5399807cf
FrancisChDour <fdourlensmonchy@gmail.com>
2021-06-08 12:06:24 +0200

    avance ton char

--- avance_ton_char_bidasse.py
+++ avance_ton_char_bidasse.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import serial
 import time
+import sys
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
     ser.flush()
-    time.sleep(3)
-    ser.write(b"200,200")
-    time.sleep(3)
-    ser.write(b"100,100")
+    ser.write("{},{}".format(sys.argv[1], sys.argv[2]).encode('utf-8'))
