3ccc960684cf7fc99f0c32caa352cd9a38a2aff7
FDOURL28 <francois.dourlensmonchy@decathlon.com>
2021-06-09 15:27:14 +0200

    git

--- vroumvroum.py
+++ vroumvroum.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
+from marvelmind import MarvelmindHedge
+from time import sleep
+import sys
 import serial
+from math import cos, sin, acos, sqrt, degrees, pi
 
+HEDGE_ID = 50
+
+hedge = None
 ser = None
 
+def readGPS():
+    position = hedge.position()
+    while position[0] != HEDGE_ID:
+        position = hedge.position()
+    return position
+
 def setup():
+    global hedge
     global ser
+    hedge = MarvelmindHedge(tty="/dev/ttyACM0", adr=None, debug=False)  # create MarvelmindHedge thread
+    if (len(sys.argv) > 1):
+        hedge.tty = sys.argv[1]
+    hedge.start()
     ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
     ser.flush()
 
@@ -15,6 +33,11 @@ def move(v1, v2):
 left = 0
 right = 0
 setup()
+
+def teta(xa, ya, xb, yb):
+    x = xb - xa
+    y = yb - ya
+    return degrees(acos(y/sqrt(x**2+y**2)) - (pi/2))
 
 while True:
     k = input()
@@ -38,3 +61,10 @@ while True:
         right-=50
         print(left, right)
         move(left, right)
+    if(k == "a"):
+        left=0
+        right=0
+        print(left, right)
+        move(left, right)
+    if(k == "e"):
+        print(readGPS())
